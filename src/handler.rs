323e68eff3a0e0864cbffc257580ff751b463cef
Maksym Vorobiov <maxim.vorobjov@gmail.com>
2020-01-18 22:09:34 +0200

    fix formatting

--- src/handler.rs
+++ src/handler.rs
@@ -1,4 +1,4 @@
-use evmap::{ShallowCopy};
+use evmap::ShallowCopy;
 
 pub struct Handler {
     pub id: u32,
@@ -15,11 +15,11 @@ impl Eq for Handler {}
 impl ShallowCopy for Handler {
     // It should be quite efficient - copy performed only on first write
     // Later evmap operates with 2 copies one for read another for write and oplog
-     unsafe fn shallow_copy(&mut self) -> Self {
-         Handler {
+    unsafe fn shallow_copy(&mut self) -> Self {
+        Handler {
             id: self.id,
             path: self.path.shallow_copy(),
-            topic: self.topic.shallow_copy()
-         }
-     }
+            topic: self.topic.shallow_copy(),
+        }
+    }
 }
--- src/router.rs
+++ src/router.rs
@@ -1,55 +1,47 @@
 use crate::Handler;
 use async_trait::async_trait;
+use evmap::{ReadHandle, WriteHandle};
 use hyper::{Body, Error, Method, Request, Response};
-use evmap::{WriteHandle, ReadHandle};
 
 type ReadMap = ReadHandle<u32, Handler>;
 type WriteMap = WriteHandle<u32, Handler>;
 
 #[derive(Clone)]
 pub struct EvmapRouter {
-	// evmap read handles
-	reader: ReadMap
+    // evmap read handles
+    reader: ReadMap,
 }
 
 /// Routes writer - only single instance of writer per map of routes exists
-/// - should it be one instance per one client? 
+/// - should it be one instance per one client?
 /// 	-- in this case domain matching should be performed before?
 /// 	-- if single instance per server then domain param should be added to handler structure
 /// 	-- if single instance per server there will be some redundancy when copying evmap_reader
 /// - add/remove routes
 pub struct EvmapRouterWriter {
-	// evmap write handle
-	writer: WriteMap 
+    // evmap write handle
+    writer: WriteMap,
 }
 
 impl EvmapRouterWriter {
-	pub fn new(writer: WriteHandle<u32, Handler>) -> Self {
-		Self {
-			writer
-		}
-	}
-    pub fn add(&mut self, method: Method, route_pattern: &str, handler: Handler) {
-
-	}
-    pub fn remove(&mut self, route_pattern: &str) {
-
-	}
+    pub fn new(writer: WriteHandle<u32, Handler>) -> Self {
+        Self { writer }
+    }
+    pub fn add(&mut self, method: Method, route_pattern: &str, handler: Handler) {}
+    pub fn remove(&mut self, route_pattern: &str) {}
 }
 
 /// Thread safe router
 /// - handle routes
 /// - safe to clone between threads (it should only make shallow copy of evmap reader)
 #[async_trait]
-pub trait Router: Send+Sync+Clone {
+pub trait Router: Send + Sync + Clone {
     async fn handle(&self, req: Request<Body>) -> Result<Response<Body>, Error>;
 }
 
 impl EvmapRouter {
     pub fn new(reader: ReadHandle<u32, Handler>) -> Self {
-        Self {
-			reader
-		}
+        Self { reader }
     }
 }
 
@@ -57,10 +49,10 @@ impl EvmapRouter {
 impl Router for EvmapRouter {
     async fn handle(&self, req: Request<Body>) -> Result<Response<Body>, Error> {
         let response = async {
-			// TODO: 
-			// let topic = self.evmap_matcher.match(req.path()).get_topic();
-			// nats.post(topic, req.body()).await
-			// nats.get().await
+            // TODO:
+            // let topic = self.evmap_matcher.match(req.path()).get_topic();
+            // nats.post(topic, req.body()).await
+            // nats.get().await
             "Hello, World".into()
         }
         .await;
